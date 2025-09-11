346dbc2516c8a542b57cbd5b46660188a5a44e48
marcolsd20 <marcondealviss@gmail.com>
2025-09-10 22:34:40 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // config.js
-export const SUPABASE_URL = process.env.SUPABASE_URL;
-export const SUPABASE_KEY = process.env.SUPABASE_KEY;
+export const SUPABASE_URL = window.SUPABASE_URL || "https://jyhemlmxsoqheirmivdn.supabase.co";
+export const SUPABASE_KEY = window.SUPABASE_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp5aGVtbG14c29xaGVpcm1pdmRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc1NDUzNjAsImV4cCI6MjA3MzEyMTM2MH0.nXhG0EV5FkSeZe1v9Tgf2Jc2KmIL4ixplgGWUJ_NTY4";
